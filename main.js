c6461d81676318eb330371f7db3f96a1931bedeb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-12 21:09:44 -0500

    Added logging.
    I added "morgan" to log the HTTP requests to a log file and to stdout.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const fs = require('fs')
+const morgan = require('morgan')
+
 const app = express()
+
+/*
+ * 'morgan' is used to log the HTTP requests that this server gets. When used
+ * this way, it will log to both a file and standard output.
+ */
+app.use(morgan('common', {
+  stream: fs.createWriteStream('./access.log', { flags: 'a' })
+}))
+app.use(morgan('dev'))
 
 /*
  * Create a static route. Everything in the 'public' directory will be sent
